982aeeb72198a3f5e08f1d6ce604726dac6748f8
nirmit1509 <nirmitsabharwal15@gmail.com>
2020-10-17 00:00:19 +0530

    Search button

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import './Navbar.css';
+import logo from '../logo192.png';
 import PersonIcon from '@material-ui/icons/Person';
 
 function Navbar() {
     return (
         <div className="navbar">
+            <img 
+                id="logo" 
+                alt="GetWork Logo" 
+                src={logo}
+            ></img>
             <h2 id="nav"><PersonIcon /> Nirmit Sabharwal</h2>
         </div>
     )
